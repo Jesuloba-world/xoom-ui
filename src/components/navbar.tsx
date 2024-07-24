acfa23d6cfdb0b9e1c1786ba68f1150668717f93
jesuloba <jesulobajohn@gmail.com>
2024-07-24 10:23:03 +0100

    start working on auth with logto

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import { MobileNav } from "./mobileNav";
+
 export const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+			<Link href={"/"} className="flex items-center gap-1">
+				<Image
+					src={"/icons/logo.svg"}
+					width={32}
+					height={32}
+					alt={"Xoom logo"}
+					className="max-sm:size-10"
+				/>
+				<p className="text-[26px] font-extrabold text-white max-sm:hidden">
+					Xoom
+				</p>
+			</Link>
+			<div className="flex justify-between gap-5">
+				{/* User Avatar - User management  */}
+				<MobileNav />
+			</div>
+		</nav>
+	);
 };
